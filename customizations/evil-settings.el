606c5ec84dfc049545767f631c3aec56b47ffd83
giulio <giuliop@gmail.com>
2015-04-28 17:23:26 +0200

    not tracking elpa dir and added two new files

---
+++ customizations/evil-settings.el
@@ -0,0 +1,4 @@
+(define-key evil-visual-state-map (kbd "C-c") 'evil-normal-state)
+(define-key evil-visual-state-map (kbd "C-c") 'evil-exit-visual-state)
+(define-key evil-normal-state-map (kbd ",,") 'evil-buffer)
+(define-key evil-normal-state-map (kbd "q") nil)
